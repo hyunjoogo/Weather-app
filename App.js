d4f057a729385fbd218161d3e819ab47a6a89ceb
HyunJoo Kim <hyunjoogo@gmail.com>
2022-01-11 18:22:38 +0900

    #3.6 Delete

--- App.js
+++ App.js
@@ -1,24 +1,59 @@
-import {StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView, Alert, Button} from 'react-native';
 import {theme} from "./color";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const STORAGE_KEY = "@toDos"
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+
+  useEffect(() => {
+    loadToDos();
+  }, [])
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
-  const onChangeText = (payload) => setText(payload)
-  const addToDo = () => {
+  const onChangeText = (payload) => setText(payload);
+  const saveToDos = async (toSave) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave))
+    } catch (e) {
+      alert("Sorry")
+    }
+  };
+  const loadToDos = async () => {
+    try {
+      const s = await AsyncStorage.getItem(STORAGE_KEY)
+      setToDos(JSON.parse(s));
+    } catch (e) {
+      alert("Sorry")
+    }
+  };
+
+  const addToDo = async () => {
     if (text === "") {
       return;
     }
-    const newToDos = Object.assign({}, toDos, {
-      [Date.now()] : {text, work : working},
-    })
+    const newToDos = {...toDos, [Date.now()]: {text, working},}
     setToDos(newToDos);
+    await saveToDos(newToDos);
     setText("");
+  }
+  const deleteTodo = (key) => {
+    Alert.alert("지우시겠습니까?", "작고 소중한 데이터가 안녕이됩니다.", [
+      {text: "취소"}, {
+        text: "확인", onPress: () => {
+          const newToDos = {...toDos}
+          delete newToDos[key]
+          setToDos(newToDos);
+          saveToDos(newToDos);
+        }
+      }
+    ]);
+
   }
   return (
     <View style={styles.container}>
@@ -39,8 +74,19 @@ export default function App() {
           style={styles.input}
           returnKeyType="done"
           placeholder={working ? "할 일을 입력하세요" : "어디로 가고 싶습니까?"}
-
         />
+        <ScrollView>
+          {Object.keys(toDos).map(key =>
+            toDos[key].working === working ? (
+              <View style={styles.toDo} key={key}>
+                <Text style={styles.toDoText}>{toDos[key].text}</Text>
+                <TouchableOpacity onPress={() => deleteTodo(key)}>
+                  <Text>❌</Text>
+                </TouchableOpacity>
+              </View>
+            ) : null
+          )}
+        </ScrollView>
       </View>
     </View>
   );
@@ -68,6 +114,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 20,
     marginTop: 20,
+    marginBottom: 10,
     fontSize: 18,
   },
+  toDo: {
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
+  }
 });
