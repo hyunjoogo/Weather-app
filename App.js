310ca1d085bca07c60f4c9c3a11eab0fa6597ed3
HyunJoo Kim <hyunjoogo@gmail.com>
2022-01-11 17:24:56 +0900

    #3.4 Paint To Dos

--- App.js
+++ App.js
@@ -1,15 +1,73 @@
-import {StyleSheet, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {theme} from "./color";
+import {useState} from "react";
 
 
 export default function App() {
-
-
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload)
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()] : {text, work : working},
+    })
+    setToDos(newToDos);
+    setText("");
+  }
   return (
     <View style={styles.container}>
+      <StatusBar style="auto"/>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color: working ? "white" : theme.grey}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color: working ? theme.grey : "white"}}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onSubmitEditing={addToDo}
+          onChangeText={onChangeText}
+          value={text}
+          style={styles.input}
+          returnKeyType="done"
+          placeholder={working ? "할 일을 입력하세요" : "어디로 가고 싶습니까?"}
 
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.background,
+    paddingHorizontal: 40,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 44,
+    fontWeight: "600",
+    color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginTop: 20,
+    fontSize: 18,
+  },
 });
